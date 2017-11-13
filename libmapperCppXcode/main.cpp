065e5fbbe68897fb49f821ab482fbbd4efce9605
johnty <johntywang@gmail.com>
2017-11-13 13:36:48 -0500

    Initial Commit

---
+++ libmapperCppXcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  libmapperCppXcode
+//
+//  Created by johnty on 2017-11-13.
+//  Copyright © 2017 johnty. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
